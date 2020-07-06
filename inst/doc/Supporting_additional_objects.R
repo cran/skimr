ba8506a06bea2d4bb5c8957f33037726f732cbd6
Elin Waring <elin.waring@gmail.com>
2020-07-06 04:10:03 +0000

    version 2.1.2

--- inst/doc/Supporting_additional_objects.R
+++ inst/doc/Supporting_additional_objects.R
@@ -6,20 +6,24 @@ nc <- st_read(system.file("shape/nc.shp", package = "sf"))
 ## -----------------------------------------------------------------------------
 class(nc)
 
+class(nc$geometry)
+
 ## -----------------------------------------------------------------------------
 skim(nc$geometry)
 
 ## -----------------------------------------------------------------------------
-funny_sf <- function(x) {
-  length(x) + 1
+get_crs <- function(column) {
+  crs <- sf::st_crs(column)
+
+  paste0("epsg: ", crs[["epsg"]], " proj4string: '", crs[["proj4string"]], "'")
 }
 
 ## -----------------------------------------------------------------------------
 skim_sf <- skim_with(
-  sfc_MULTIPOLYGON = sfl(
+  sfc = sfl(
     n_unique = n_unique,
     valid = ~ sum(sf::st_is_valid(.)),
-    funny = funny_sf
+    crs = get_crs
   )
 )
 
@@ -27,56 +31,20 @@ skim_sf <- skim_with(
 skim_sf(nc$geometry)
 
 ## -----------------------------------------------------------------------------
-skim_by_type.sfc_MULTIPOLYGON <- function(mangled, columns, data) {
-  skimmed <- dplyr::summarize_at(data, columns, mangled$funs)
-  build_results(skimmed, columns, NULL)
-}
-
-## -----------------------------------------------------------------------------
-skim_sf(nc)
-
-## -----------------------------------------------------------------------------
-#' Skimming functions for `sfc_MULTIPOLYGON` objects.
-#' @export
-skim_sf <- skim_with(
-  sfc_MULTIPOLYGON = sfl(
-    missing = n_missing,
-    n = length,
-    n_unique = n_unique,
-    valid = ~ sum(sf::st_is_valid(.)),
-    funny = funny_sf
-  )
-)
-
-#' A skim_by_type function for `sfc_MULTIPOLYGON` objects.
-#' @export
-skim_by_type.sfc_MULTIPOLYGON <- function(mangled, columns, data) {
-  skimmed <- dplyr::summarize_at(data, columns, mangled$funs)
-  skimr::build_results(skimmed, columns, NULL)
-}
-
-## -----------------------------------------------------------------------------
 #' @importFrom skimr get_skimmers
 #' @export
-get_skimmers.sfc_MULTIPOLYGON <- function(column) {
+get_skimmers.sfc <- function(column) {
   sfl(
-    skim_type = "sfc_MULTIPOLYGON",
+    skim_type = "sfc",
     n_unique = n_unique,
     valid = ~ sum(sf::st_is_valid(.)),
-    funny = funny_sf
-  )
-}
-
-## -----------------------------------------------------------------------------
-#' @export
-get_skimmers.sfc_POINT <- function(column) {
-  sfl(
-    skim_type = "sfc_POINT",
-    n_unique = n_unique,
-    valid = ~ sum(sf::st_is_valid(.))
+    crs = get_crs
   )
 }
 
 ## -----------------------------------------------------------------------------
 get_default_skimmer_names()
+
+## -----------------------------------------------------------------------------
+skim(nc)
 
